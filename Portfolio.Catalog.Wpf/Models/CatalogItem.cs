28abbcb1c6b68719f81150d1af42ede4c9e36204
jacobsweaver96 email = jacobsweaver@gmail.com <jacobsweaver@gmail.com>
2023-11-26 23:46:53 -0500

    Added catalog item view;

---
+++ Portfolio.Catalog.Wpf/Models/CatalogItem.cs
@@ -0,0 +1,9 @@
+﻿namespace Portfolio.Catalog.Wpf.Models
+{
+    public class CatalogItem
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
---
+++ Portfolio.Catalog.Wpf/ViewModels/CatalogViewModel.cs
@@ -0,0 +1,37 @@
+﻿using Portfolio.Catalog.Wpf.Models;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Portfolio.Catalog.Wpf.ViewModels
+{
+    public class CatalogViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        private ObservableCollection<CatalogItem> _items = new ObservableCollection<CatalogItem>
+        {
+            new CatalogItem { Name="NameA", Description="DescA" },
+            new CatalogItem { Name="NameB", Description="DescB" },
+            new CatalogItem { Name="NameC", Description="DescC" },
+            new CatalogItem { Name="NameD", Description="DescD" },
+        };
+
+        public ObservableCollection<CatalogItem> Items
+        {
+            get
+            {
+                return _items;
+            }
+            set
+            {
+                _items = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
